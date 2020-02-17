747516062f2d9b9de909390b5d702050fe5c792b
nelson1812 <nelsoncc1812@gmail.com>
2020-02-17 15:53:43 +0100

    First commit

--- HarcodedNews/routes/auth.routes.js
+++ HarcodedNews/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
-const User = require("../models/User.model");
+const User = require("../models/User.model")
+
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
